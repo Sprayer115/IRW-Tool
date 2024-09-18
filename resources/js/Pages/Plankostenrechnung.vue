25b57532e49ceb95d6622af0048e5fd55cf1ddb1
Eron <EronSejdiu@gmx.de>
2024-09-18 17:45:29 +0200

    PKR not finished

--- resources/js/Pages/Plankostenrechnung.vue
+++ resources/js/Pages/Plankostenrechnung.vue
@@ -3,6 +3,9 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { ref, computed, onMounted } from 'vue';
 import axios from 'axios';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 
 const emit = defineEmits(['update:checked']);
 
@@ -26,23 +29,39 @@ const proxyChecked = computed({
     },
 });
 
-const aufwand = ref(null);
-const zeitraum = ref(null);
-const zeitliche_abgrenzung = ref(null);
-const sachliche_abgrenzung = ref(null);
-const za_aw = ref(null);
-const kosten = ref(null);
+//Sollkosten
+const sollkosten = ref(null);
+const fixkosten = ref(null);
+const varPlanverrechnungssatz = ref(null);
+const istLeistung = ref(null);
+
+//Verbrauchsabweichung
+const verbrauchsabweichung = ref(null);
+const istKosten = ref(null);
+const sollKosten = ref(null);
+
+//Beschäftigungsabweichung
+const beschaeftAbweichung = ref(null);
+const istKostenVerechneteLeistung = ref(null);
+
+//Gesamtabweichung
+const gesamtabweichung = ref(null);
+
 const rows = ref([]);
 
 onMounted(() => {
     rows.value = props.items.map(item => ({
         id: item.id,  // Ensure you have an ID field for deleting rows later
-        aufwand: item.aufwand,
-        zeitraum: item.zeitraum,
-        zeitliche_abgrenzung: item.zeitliche_abgrenzung,
-        sachliche_abgrenzung: item.sachliche_abgrenzung,
-        za_aw: item.za_aw,
-        kosten: item.kosten,
+        sollkosten: item.sollkosten,
+        fixkosten: item.fixkosten,
+        varPlanverrechnungssatz: item.varPlanverrechnungssatz,
+        istLeistung: item.istLeistung,
+        verbrauchsabweichung: item.verbrauchsabweichung,
+        istKosten: item.istKosten,
+        sollKosten: item.sollKosten,
+        beschaeftAbweichung: item.beschaeftAbweichung,
+        istKostenVerechneteLeistung: item.istKostenVerechneteLeistung,
+        gesamtabweichung: item.gesamtabweichung,
         neueSpalte: null,
     }));
 });
@@ -50,27 +69,27 @@ onMounted(() => {
 async function addRow() {
     try {
         const newRow = {
-            aufwand: aufwand.value,
-            zeitraum: zeitraum.value,
-            zeitliche_abgrenzung: zeitliche_abgrenzung.value,
-            sachliche_abgrenzung: sachliche_abgrenzung.value,
-            za_aw: za_aw.value,
-            kosten: kosten.value,
+            sollkosten: sollkosten.value,
+            fixkosten: fixkosten.value, // Use month difference
+            varPlanverrechnungssatz: varPlanverrechnungssatz.value,
+            istLeistung: istLeistung.value,
+            verbrauchsabweichung: verbrauchsabweichung.value,
+            istKosten: istKosten.value,
+            sollKosten: sollKosten.value,
+            beschaeftAbweichung: beschaeftAbweichung.value,
+            istKostenVerechneteLeistung: istKostenVerechneteLeistung.value,
+            gesamtabweichung: gesamtabweichung.value,
         };
 
         try{
-            newRow.zeitliche_abgrenzung = calculateZA(newRow);
-            newRow.za_aw = calculateZAAW(newRow);
-            newRow.kosten = calculateWert(newRow);
+            newRow.sollKosten = calculateSK(newRow);
+            newRow.verbrauchsabweichung = calculateVA(newRow);
+            newRow.beschaeftAbweichung = calculateBA(newRow);
+            newRow.gesamtabweichung = calculateGA(newRow);
             console.log(newRow);
-            if(newRow.sachliche_abgrenzung == ''){
-                newRow.sachliche_abgrenzung = '-';
-            }
         }catch(error){
             console.error('Error setting functions:', error)
         }
-
-        // plankosten_variabel, plankosten_gesamt, istkosten, stueck_soll, stueck_ist als newRow
         
 
         const response = await axios.post('/plankostenrechnung', newRow);
@@ -102,46 +121,48 @@ async function deleteRow(id, index) {
 
 function clearInputs() {
     aufwand.value = null;
-    zeitraum.value = null;
+    selectedRange.value = null;
     zeitliche_abgrenzung.value = null;
     sachliche_abgrenzung.value = null;
     za_aw.value = null;
     kosten.value = null;
 }
 
-function calculateWert(row) {
+function calculateBA(row) {
     let wert = 0;
-    if (row.zeitraum && row.aufwand) {
-        wert = (row.aufwand / 12) * row.zeitraum - row.aufwand;
-    } else if (row.sachliche_abgrenzung) {
-        wert = row.sachliche_abgrenzung * 1.2;
+    if (row.sollKosten && row.istKostenVerechneteLeistung) {
+        wert = row.sollKosten - row.istKostenVerechneteLeistung;
     }
-    row.neueSpalte = wert;
+    row.beschaeftAbweichung = wert;
     return wert;
 }
 
-function calculateZA(row){
+function calculateGA(row) {
     let wert = 0;
-    if(row.zeitliche_abgrenzung)
-    {
-        wert = zeitliche_abgrenzung;
+    if (row.beschaeftAbweichung && row.verbrauchsabweichung) {
+        wert = row.beschaeftAbweichung - row.verbrauchsabweichung;
     }
-    else if(row.aufwand && row.zeitraum)
-    {
-        let x = row.aufwand / 12;
-        wert = x * row.zeitraum;
-    }
-    row.zeitliche_abgrenzung = -(wert);
-    return -(wert);
+    row.gesamtabweichung = wert;
+    return wert;
 }
 
-function calculateZAAW(row){
+function calculateSK(row){
     let wert = 0;
-    if(row.aufwand && row.zeitliche_abgrenzung)
+    if(row.fixkosten && row. varPlanverrechnungssatz && row.istLeistung)
     {
-        wert = row.aufwand + row.zeitliche_abgrenzung;
+        wert = row.fixkosten + (row.varPlanverrechnungssatz * istLeistung);
     }
-    row.za_aw = wert;
+    row.sollKosten = wert;
+    return wert;
+}
+
+function calculateVA(row){
+    let wert = 0;
+    if(row.istKosten && row.sollKosten)
+    {
+        wert = row.istKosten - row.sollKosten;
+    }
+    row.verbrauchsabweichung = wert;
     return wert;
 }
 </script>
@@ -150,52 +171,97 @@ function calculateZAAW(row){
 
 <template>
     <div>
-      <Head title="plankostenrechnung">
+      <Head title="Plankostenrechnung">
       </Head>
   
       <AuthenticatedLayout>
           <template #header>
-              <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">plankostenrechnung</h2>
+              <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Plakostenrechnung</h2>
           </template>
-          
-          <div class="py-12">
-              <div class="input-area">
-                  <label for="input1">Aufwand:</label>
-                  <input type="number" v-model="aufwand" id="input1" />
-                  
-                  <label for="input2">Zeitraum (Monate):</label>
-                  <input type="number" v-model="zeitraum" id="input2" />
+          <div class="py-12 m-lg-4">
+            <div class="input-area"
+                style="display: grid; grid-template-columns: 3fr 2fr; grid-gap: 20px; align-items: center; justify-content: center; justify-items: center; max-width: 600px; margin: 0 auto;">
 
-                  <label for="input4">Sachliche Abgrenzung:</label>
-                  <input type="number" v-model="sachliche_abgrenzung" id="input4" />
+                <!-- Label and Date Picker in Grid -->
+                <label for="input1" style="justify-self: start;">Fixkosten:
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input1" style="max-width: 250px;" />
 
-                  <label for="input6">Kosten:</label>
-                  <input type="number" v-model="kosten" id="input6" />
-                  
-                  <button class="button" @click="addRow"> Add Row </button>
-              </div>
+                <!-- variabler Planverrechnungssatz Input -->
+                <label for="input2" style="justify-self: start;">variabler Planverrechnungssatz:  
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input2" style="max-width: 250px;" />
+                
+                <!-- IstLeistung Input -->
+                <label for="input3" style="justify-self: start;">Ist Leistung:  
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input3" style="max-width: 250px;" />
+
+                <!-- IstKosten Input -->
+                <label for="input4" style="justify-self: start;">Ist Kosten:  
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input4" style="max-width: 250px;" />
+
+                <!-- sollKosten Input -->
+                <label for="input5" style="justify-self: start;">Soll Kosten:  
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input5" style="max-width: 250px;" />
+
+                <!-- istKostenVerrechneteLeistung Input -->
+                <label for="input1" style="justify-self: start;">IstKosten verrechnete Leistung:  
+                    <span id="tooltip-aufwand" class="tooltip-container">
+                        <sup class="information">i</sup>
+                        <span class="tooltip-text">Definition für den Aufwand</span>
+                    </span>
+                </label>
+                <input type="number" v-model="aufwand" id="input6" style="max-width: 250px;" />
+
+                <!-- Empty space to align button -->
+                <div></div>
+                <button class="button bg-primary" style="justify-self: start; max-width: 150px;" @click="addRow">Berechnen</button>
+            </div>
+
+
+
+
   
               <table class="table">
                   <thead>
                       <tr>
-                          <th class="success">Aufwand</th>
-                          <th class = "highlight">Zeitraum</th>
-                          <th>Zeitliche Abgrenzung</th>
-                          <th>Sachliche Abgrenzung</th>
-                          <th>zeitlich abgegrenzter Aufwand</th>
-                          <th class="warning">Kosten</th>
-                          <th>Actions</th>
+                            <th class="bg-success">SollKosten</th>
+                            <th class = "bg-warning">Verbrauchsabweichung</th>
+                            <th>Beschäftigungsabweichung</th>
+                            <th>Gesamtabweichung</th>
+                            <th>Actions</th>
                       </tr>
                   </thead>
                   <tbody>
                       <tr v-for="(row, index) in rows" :key="index">
-                          <td>{{ row.aufwand }}</td>
-                          <td>{{ row.zeitraum }}</td>
-                          <td>{{ row.zeitliche_abgrenzung }}</td>
-                          <td>{{ row.sachliche_abgrenzung }}</td>
-                          <td>{{ row.za_aw }}</td>
-                          <td>{{ row.kosten }}</td>
-                          <td><button class = "button" @click="deleteRow(row.id, index)">Delete</button></td>
+                          <td>{{ row.sollKosten }}</td>
+                          <td>{{ row.verbrauchsabweichung }}</td>
+                          <td>{{ row.beschaeftAbweichung }}</td>
+                          <td>{{ row.gesamtabweichung }}</td>
+                          <td><button class = "button bg-danger" @click="deleteRow(row.id, index)">löschen</button></td>
                       </tr>
                   </tbody>
               </table>
@@ -216,17 +282,6 @@ th, td {
     text-align: left;
 }
 
-.highlight {
-    background-color: yellow;
-}
-
-.success{
-    background-color: green;
-}
-
-.warning{
-    background-color: red;
-}
 
 .input-area {
     margin-bottom: 20px;
@@ -288,4 +343,48 @@ button {
     visibility: visible;
     opacity: 1;
 }
+
+.tooltip-container {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+}
+
+.information {
+    border: 1px solid;
+    border-radius: 50%;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 10px;
+    height: 10px;
+    font-size: 8px;
+    margin-left: 1px;
+}
+
+/* Tooltip text - hidden by default */
+.tooltip-text {
+    visibility: hidden;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    border-radius: 5px;
+    padding: 5px;
+    position: absolute;
+    z-index: 1;
+    bottom: 150%; /* Adjust to position above the element */
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 0;
+    transition: opacity 0.5s ease-in-out; /* Animation */
+    white-space: nowrap;
+}
+
+/* Show the tooltip when hovering over the container */
+.tooltip-container:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+    transition-delay: 0.1s; /* Delay before showing the tooltip */
+}
+
 </style>
