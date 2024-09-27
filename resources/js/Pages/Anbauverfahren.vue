cb3d7f2b6a86c619ada0a5ce2777d952b46f24e4
S.Driescher <simondriescher@yahoo.de>
2024-09-27 12:14:51 +0200

    fucking innerbetriebliche

--- resources/js/Pages/Anbauverfahren.vue
+++ resources/js/Pages/Anbauverfahren.vue
@@ -10,8 +10,12 @@ const props = defineProps({
 const emit = defineEmits(['calculate']);
 
 const performCalculations = () => {
+    console.log("PreAuxiliaryCostCenters:", props.preAuxiliaryCostCenters);
+    console.log("primaryOverheadCosts", props.primaryOverheadCosts);
+    console.log("allocationMatrix", props.allocationMatrix);
+
     const results = {};
-    
+
     // Initialize results object
     props.preAuxiliaryCostCenters.forEach((cc) => {
         results[cc.id] = {
@@ -35,25 +39,27 @@ const performCalculations = () => {
 
     // Calculate allocations
     props.preAuxiliaryCostCenters.forEach((cc) => {
-        const totalUnits = props.primaryOverheadCosts.reduce((sum, oc) => 
-            sum + (props.allocationMatrix[cc.id]?.[oc.id] || 0), 0);
+        const totalUnits = Object.values(props.allocationMatrix[cc.id]).reduce((sum, value) => sum + value, 0);
 
-        props.primaryOverheadCosts.forEach((oc) => {
-            const units = props.allocationMatrix[cc.id]?.[oc.id] || 0;
+        Object.entries(props.allocationMatrix[cc.id]).forEach(([targetId, units]) => {
             if (totalUnits > 0) {
                 const allocationAmount = cc.value * (units / totalUnits);
-                results[cc.id].allocations[`oc${oc.id}`] = allocationAmount;
-                results[`oc${oc.id}`].secondaryCosts += allocationAmount;
-                results[`oc${oc.id}`].totalCosts += allocationAmount;
+                results[cc.id].allocations[targetId] = allocationAmount;
+
+                if (targetId.startsWith('oc')) {
+                    results[targetId].secondaryCosts += allocationAmount;
+                    results[targetId].totalCosts += allocationAmount;
+                } else {
+                    results[targetId].secondaryCosts += allocationAmount;
+                }
             }
         });
-    });
 
-    // Set Vor-Hilfskostenstellen total costs to 0 after allocation
-    props.preAuxiliaryCostCenters.forEach((cc) => {
+        // Set total costs to 0 after allocation
         results[cc.id].totalCosts = 0;
     });
 
+    console.log(results);
     emit('calculate', results);
 };
 
